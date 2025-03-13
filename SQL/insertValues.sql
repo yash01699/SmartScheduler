d6750a55d876cf36991ea3d37997ee62b672e761
Yashasvi Nancherla <67097796+yash01699@users.noreply.github.com>
2025-03-13 23:08:31 +0530

    Create insertValues.sql

---
+++ SQL/insertValues.sql
@@ -0,0 +1,5 @@
+INSERT INTO ToDoItem (Id, Name, IsComplete, Time, Priority) VALUES
+(1, 'read a script', 0, 1, 4),
+(2, 'write a script', 0, 2, 5),
+(6, 'movie watching', 0, 3, 2),
+(7, 'babysit niece', 0, 1, 5);
