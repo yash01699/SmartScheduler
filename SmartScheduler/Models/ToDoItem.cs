96b91b7322c34736fd690feeb3e029038f6534eb
Yashasvi Nancherla <67097796+yash01699@users.noreply.github.com>
2025-03-13 22:24:04 +0530

    Add files via upload

---
+++ SmartScheduler/Models/ToDoItem.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+namespace SmartScheduler.Models;
+
+
+[Table("ToDoItem")]
+
+public record TodoItem
+{
+    public TodoItem() { }
+
+    public TodoItem(int id, string name, bool isComplete, int priority, int time)
+    {
+        Id = id;
+        Name = name;
+        IsComplete = isComplete;
+        Priority = priority;
+        Time = time;
+    }
+
+    [Key]
+    [Column("Id")]
+    public int Id { get; init; }
+    [Column("Name")]
+    public string? Name { get; init; }
+    [Column("IsComplete")]
+    public bool IsComplete { get; init; }
+    [Column("Time")]
+    public int Time { get; init; }
+    [Column("Priority")]
+    public int Priority { get; init; }
+}
