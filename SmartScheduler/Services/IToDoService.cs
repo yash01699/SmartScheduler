f4c47cdcf2582f59474b5e8533f78df465229e45
Yashasvi Nancherla <67097796+yash01699@users.noreply.github.com>
2025-03-13 22:26:32 +0530

    Add files via upload

---
+++ SmartScheduler/Services/IToDoService.cs
@@ -0,0 +1,12 @@
+﻿using SmartScheduler.Models;
+
+namespace SmartScheduler.Services;
+
+public interface ITodoService
+{
+    IEnumerable<TodoItem> GetTodos();
+    TodoItem GetTodo(int id);
+    TodoItem AddTodo(TodoItem todoItem);
+    int UpdateTodo(TodoItem todoItem);
+    void DeleteTodo(TodoItem todoItem);
+}
---
+++ SmartScheduler/Services/ToDoService.cs
@@ -0,0 +1,39 @@
+﻿using SmartScheduler.Models;
+using SmartScheduler.Repositories;
+
+namespace SmartScheduler.Services;
+
+public class TodoService : ITodoService
+{
+    private readonly ITodoRepository _repo;
+
+    public TodoService(ITodoRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public IEnumerable<TodoItem> GetTodos()
+    {
+        return _repo.GetTodos();
+    }
+
+    public TodoItem GetTodo(int id)
+    {
+        return _repo.GetTodo(id);
+    }
+
+    public TodoItem AddTodo(TodoItem todoItem)
+    {
+        return _repo.AddTodo(todoItem);
+    }
+
+    public int UpdateTodo(TodoItem todoItem)
+    {
+        return _repo.UpdateTodo(todoItem);
+    }
+
+    public void DeleteTodo(TodoItem todoItem)
+    {
+        _repo.DeleteTodo(todoItem);
+    }
+}
